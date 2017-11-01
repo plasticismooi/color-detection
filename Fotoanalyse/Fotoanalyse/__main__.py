95182f08ae82b600d42fee96cc6a26e805fe5311
spagoud <tdlandzaat@gmail.com>
2017-11-01 16:33:21 +0100

    working programm with OOP

--- Fotoanalyse/Fotoanalyse/__main__.py
+++ Fotoanalyse/Fotoanalyse/__main__.py
@@ -1,6 +1,6 @@
 # Tom Landzaat student @ EE THUAS
 # student ID : 14073595
-# date : 31-10-2017
+# date : 1-11-2017
 
 import numpy as np
 import cv2
@@ -9,6 +9,7 @@ from time import sleep
 import matplotlib
 from matplotlib import pyplot as plt
 import math
+from color_detection import color_detection
 
 def setup_camera(FotoNumber):
     
@@ -28,63 +29,30 @@ def setup_camera(FotoNumber):
     #2.05 is de totale tijd die de lopendeband nodig heeft om nieuw plastic onder de camera te leggen
     sleep(2.05)
     
-def color_detection(FotoNumber):
-    print('start color detection')
-    #inlezen fotos
-    image = cv2.imread('C:\\Users\\tom_l\\Desktop\\School\\HHS\\Jaar_3\\Stage_1\\fotos\\plastic_4.png')
-    LAB_image = cv2.cvtColor(image, cv2.COLOR_BGR2LAB)
-    #deel van de foto is niet op van lopende band, deze moet weggeknipt worden
-    #image = image[55:768, 0:1024]
 
-    zwart = 0
-    kleur = 0
-    wit = 0
-    pixels_totaal = 0
+image1 = cv2.imread('C:\\Users\\tom_l\\Desktop\\School\\HHS\\Jaar_3\\Stage_1\\fotos\\plastic_4.png')
+image2 = cv2.imread('C:\\Users\\tom_l\\Desktop\\School\\HHS\\Jaar_3\\Stage_1\\fotos\\plastic_6.png')
+
+#image_1 instellen als object
+image_1 = color_detection(image1, 155)
+image_1.Detect()
+image_1.PrintTotalPixels()
+image_1.PrintBoundary()
+image_1.PrintPercentages()
+
+image_2 = color_detection(image2, 154)
+image_2.Detect()
+image_2.PrintTotalPixels()
+image_2.PrintBoundary()
+image_2.PrintPercentages()
+
+
+
+
+
+
+
+
+
     
-    height, width, channels = image.shape
 
-    for LoopVariableY in range(height - 1):
-        for LoopVariableX in range(width - 1):
-
-            BGR_array = image[LoopVariableY,LoopVariableX]
-            if (((BGR_array[0]**2) + (BGR_array[1]**2) + (BGR_array[2]**2)) > 155**2):
-          
-                LAB_array = LAB_image[LoopVariableY,LoopVariableX]
-
-                #assenstelsel aanpassen naar 0-100
-                LAB_array[0] = (LAB_array[0] * 0.392)
-
-                if ( LAB_array[0] < 21):
-                    zwart += 1
-                elif ((LAB_array[0] > 20) & (LAB_array[0] < 70)):
-                   kleur += 1
-                elif ( LAB_array[0] >= 70 ):
-                    wit += 1
-                  
-    pixels_totaal = wit + zwart + kleur
-     #wanneer het voorkomt dat er geen plastic op de band ligt, zijn er geen pixels, dus zal er door nul gedeeld worden, dit moet voorkomen worden
-               
-    if (pixels_totaal != 0): 
-        proc_zwart = (zwart / pixels_totaal) * 100
-        proc_kleur = (kleur / pixels_totaal) * 100
-        proc_wit = (wit / pixels_totaal) * 100 
-        
-        print('foto %i'%FotoNumber )
-        print(proc_wit)
-        print('procent is wit''\n')
-        print(proc_kleur)
-        print('procent is kleur''\n')
-        print(proc_zwart)
-        print('procent is zwart''\n')
-    else:
-        print('er ligt geen plastic op de band bij foto %i'%FotoNumber)
-
-                               
-FotoNumber = 1
-
-#for b in range(FotoNumber):
-#   setup_camera(FotoNumber)
-    
-#foto's doorlopen die gemaakt zijn in setup_camera functie
-for b in range(FotoNumber):
-    color_detection(FotoNumber)
--- Fotoanalyse/Fotoanalyse/color_detection.py
+++ Fotoanalyse/Fotoanalyse/color_detection.py
@@ -1,23 +1,76 @@
-#class color_detection:
+import cv2
 
-#    def __init__(self, image):
-#        self.image = image
+class color_detection:
+
+    TotalPixels = 0
+    boundary = 155
+    zwart = 0
+    wit = 0
+    kleur = 0 
+
+    def __init__(self, image, boundary):
+        self.image = image
+        self.boundary = boundary
+        #detection in RGB or CIELAB?
+
+    def Detect(self):
+
+        height, width, channels = self.image.shape
+        LAB_image = cv2.cvtColor(self.image, cv2.COLOR_BGR2LAB)
+
+        for LoopVariableY in range(height):
+            for LoopVariableX in range(width):
+
+                BGR_array = self.image[LoopVariableY,LoopVariableX]
+                if (((BGR_array[0]**2) + (BGR_array[1]**2) + (BGR_array[2]**2)) > self.boundary**2):
+          
+                    LAB_array = LAB_image[LoopVariableY,LoopVariableX]
+
+                    #assenstelsel aanpassen naar 0-100
+                    LAB_array[0] = (LAB_array[0] * 0.392)
+
+                    if ( LAB_array[0] < 21):
+                        self.zwart += 1
+                    elif ((LAB_array[0] > 20) & (LAB_array[0] < 70)):
+                        self.kleur += 1
+                    elif ( LAB_array[0] >= 70 ):
+                        self.wit += 1
+        
+        TotalPixels = self.wit + self.kleur + self.zwart
+        color_detection.TotalPixels = TotalPixels
 
 
-#    def Detect(self, image):
-#        #daadwerkelijke kleuren detectie 
+    def PrintTotalPixels(self):
+        print(self.TotalPixels, '\n')
 
-#    def setBoundaries(self, boundary):
-#        #set de grenswaarde voor RGB van lopende band
+    def SetBoundary(self, boundary):
+        self.boundary = boundary
 
-#    def Addcolor(self):
-#        #kleur toevoegen jwz
+    def PrintBoundary(self):
+        print('boundary is', self.boundary, '\n')
+       
+        
+    def PrintPercentages(self):
+        if (self.TotalPixels != 0): 
 
-#    def PrintPercentages(self, ):
-#        #alle returns van alle kleuren weergeven
+            proc_zwart = (self.zwart / self.TotalPixels) * 100
+            proc_kleur = (self.kleur / self.TotalPixels) * 100
+            proc_wit = (self.wit / self.TotalPixels) * 100 
+            #print data
+            
+            print(proc_wit)
+            print('procent is wit''\n')
+            print(proc_kleur)
+            print('procent is kleur''\n')
+            print(proc_zwart)
+            print('procent is zwart''\n')
+        else:
+            print('er liggen geen plastic flakes op de foto')
 
-#    def CalcTotalPixels (self, ):
-#        #alle gedetecteerde pixels bij elkaar optellen
+
+        
+
+
 
 
 
